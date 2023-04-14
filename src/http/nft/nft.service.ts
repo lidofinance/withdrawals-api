a777009708b1eac47843e384a656f45ce833a5cc
Andrei <t.fane@mail.ru>
2023-04-14 14:54:35 +0700

    fix: add svg assets

--- src/http/nft/nft.service.ts
+++ src/http/nft/nft.service.ts
@@ -3,6 +3,7 @@ import { WithdrawalQueue, WITHDRAWAL_QUEUE_CONTRACT_TOKEN } from '@lido-nestjs/c
 import { ConfigService } from 'common/config';
 
 import { NFTDto, NFTParamsDto, NFTOptionsDto } from './dto';
+import { glyphNumbers, simpleNumbers } from './nft.assets';
 
 @Injectable()
 export class NFTService {
@@ -29,28 +30,23 @@ export class NFTService {
 
   // svg mock
   generateSvgImage(params: NFTParamsDto, query: NFTOptionsDto): string {
+    // TODO: implement svg generation
     const { status, amount, created_at } = query;
     const tokenId = Number(params.tokenId);
 
     const svgString = `
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300">
-        <rect width="100%" height="100%" fill="#${Math.floor(Math.random() * 16777215).toString(16)}" />
-        <rect x="50" y="50" width="200" height="200" fill="#${Math.floor(Math.random() * 16777215).toString(16)}" />
-        <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-size="32">NFT #${tokenId}</text>
-        <text x="50%" y="60%" dominant-baseline="middle" text-anchor="middle" font-size="16">Status: ${status}</text>
-        <text x="50%" y="70%" dominant-baseline="middle" text-anchor="middle" font-size="16">Amount: ${amount}</text>
-        <text x="50%" y="80%" dominant-baseline="middle" text-anchor="middle" font-size="16">Created: ${created_at}</text>
-        <animateTransform
-          attributeName="transform"
-          attributeType="XML"
-          type="rotate"
-          from="0 0 0"
-          to="360 0 0"
-          begin="0s"
-          dur="15s"
-          repeatCount="indefinite"
-        />
-      </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" width="2000" height="2000"
+  preserveAspectRatio="xMidYMid meet"
+  style="width: 100%; height: 100%; transform: translate3d(0px, 0px, 0px);">
+  <g clip-path="url(#__lottie_element_172)"
+  opacity="1" style="display: block;">
+         <animateTransform attributeName="transform" attributeType="XML" type="translate" from="0 0" to="0 2000" begin="0s" dur="5s" repeatCount="indefinite"></animateTransform>
+         <g transform="matrix(1,0,0,1,14,120)" opacity="1" style="display: block;">
+         ${glyphNumbers[tokenId % 10]}
+</g>
+         ${simpleNumbers[tokenId % 10]}
+         </g>
+         </svg>
     `;
 
     return svgString;
