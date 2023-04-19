11cb584e1b167117586feb528808c0c2f3c1d3e9
Andrei <t.fane@mail.ru>
2023-04-19 17:42:39 +0700

    feat: change uri format, update env sample

--- src/http/nft/dto/nft-options.dto.ts
+++ src/http/nft/dto/nft-options.dto.ts
@@ -4,22 +4,30 @@ import { IsInt, IsOptional, Min } from 'class-validator';
 import { ToLowerCase } from 'common/transforms';
 
 export class NFTOptionsDto {
-  @ApiPropertyOptional({ type: 'string', example: 'pending', description: 'Requests finalize status' })
+  @ApiPropertyOptional({
+    type: 'bigint',
+    minimum: 0,
+    example: '25000000000000000000',
+    description: 'Requested token amount',
+  })
   @ToLowerCase()
-  @IsOptional()
-  readonly status: 'finalized' | 'pending';
-
-  @ApiPropertyOptional({ type: 'bigint', minimum: 0, example: '25000000000000000000', description: 'Token amount' })
-  @Type(() => BigInt)
-  @IsInt()
-  @Min(0)
-  @IsOptional()
-  readonly amount: string;
+  readonly requested: 'finalized' | 'pending';
 
   @ApiPropertyOptional({ type: 'timestamp', example: 1658650005, minimum: 0, description: 'Created timestamp' })
   @Type(() => Number)
   @IsInt()
   @Min(0)
-  @IsOptional()
   readonly created_at: number;
+
+  @ApiPropertyOptional({
+    type: 'bigint',
+    minimum: 0,
+    example: '25000000000000000000',
+    description: 'Claimable token amount',
+  })
+  @Type(() => BigInt)
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  readonly finalized?: string;
 }
