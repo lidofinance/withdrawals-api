bc0007719ac597e90c4b95866cc34fac4c3916bb
Andrei <t.fane@mail.ru>
2023-04-24 17:13:39 +0700

    fix: fix isFinite

--- src/http/request-time/request-time.utils.ts
+++ src/http/request-time/request-time.utils.ts
@@ -2,7 +2,7 @@ import { MAX_VALID_NUMBER } from './request-time.constants';
 
 export const maxMinNumberValidation = (amount: string, min: string) => {
   if (Number.isNaN(Number(amount))) return '0';
-  if (!Number.isFinite(amount)) return '0';
+  if (Number.isFinite(amount)) return '0';
   if (Math.abs(Number(amount)) > MAX_VALID_NUMBER) return String(MAX_VALID_NUMBER);
   if (Math.abs(Number(amount)) < Number(min)) return min;
 
