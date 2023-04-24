7ed0b7e9112cfc54dc34596fbd60fd9879ec3196
Andrei <t.fane@mail.ru>
2023-04-24 15:59:54 +0700

    feat: add borders for calc request time

---
+++ src/http/request-time/request-time.utils.ts
@@ -0,0 +1,10 @@
+import { MAX_VALID_NUMBER } from './request-time.constants';
+
+export const maxMinNumberValidation = (amount: string, min: string) => {
+  if (Number.isNaN(Number(amount))) return '0';
+  if (Number.isFinite(amount)) return '0';
+  if (Math.abs(Number(amount)) > MAX_VALID_NUMBER) return String(MAX_VALID_NUMBER);
+  if (Math.abs(Number(amount)) < Number(min)) return min;
+
+  return amount;
+};
