666a5e015daf87f01e0371d22209a100fa179471
Andrei <t.fane@mail.ru>
2023-05-01 13:14:10 +0700

    feat: add prefix

--- src/common/prometheus/prometheus.module.ts
+++ src/common/prometheus/prometheus.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 import { PrometheusModule as PrometheusModuleSource } from '@willsoto/nestjs-prometheus';
-import { METRICS_URL } from './prometheus.constants';
+import { METRICS_URL, METRICS_PREFIX } from './prometheus.constants';
 import { PrometheusController } from './prometheus.controller';
 import { PrometheusService } from './prometheus.service';
 
@@ -10,7 +10,7 @@ import { PrometheusService } from './prometheus.service';
     PrometheusModuleSource.register({
       controller: PrometheusController,
       path: METRICS_URL,
-      defaultMetrics: { enabled: true },
+      defaultMetrics: { enabled: true, config: { prefix: METRICS_PREFIX } },
     }),
   ],
   providers: [PrometheusService],
