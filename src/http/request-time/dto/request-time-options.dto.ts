b2373d981e7cdd78779c0095e1dbc9b695ab3e81
Andrei <t.fane@mail.ru>
2023-05-02 20:55:07 +0700

    fix: fix validation request-time

--- src/http/request-time/dto/request-time-options.dto.ts
+++ src/http/request-time/dto/request-time-options.dto.ts
@@ -1,12 +1,10 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsInt, IsOptional, Min } from 'class-validator';
+import { IsOptional } from 'class-validator';
 
 export class RequestTimeOptionsDto {
-  @ApiPropertyOptional({ type: 'bigint', minimum: 0, description: 'stETH amount' })
+  @ApiPropertyOptional({ type: 'string', minimum: 0, description: 'stETH amount' })
   @Type(() => BigInt)
-  @IsInt()
-  @Min(0)
   @IsOptional()
   readonly amount?: string;
 }
