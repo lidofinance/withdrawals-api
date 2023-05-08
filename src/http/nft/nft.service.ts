faecaa851433988a205be89a46902ef9db1fc765
Andrei <t.fane@mail.ru>
2023-05-09 02:48:10 +0700

    fix: fix calc animation duration

--- src/http/nft/nft.service.ts
+++ src/http/nft/nft.service.ts
@@ -68,7 +68,7 @@ export class NFTService {
     const id = generateIdSvg(tokenId.toString());
     const date = generateDateSvg(created_at);
 
-    const lineAnimationDuration = 22;
+    const lineAnimationDuration = Math.floor(left.size / 70);
 
     const svgString = `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2000 2000" width="2000" height="2000"
