86b8bb1c911cd4fee107ef4df22522610116369a
Andrei <t.fane@mail.ru>
2023-05-12 21:56:08 +0700

    fix: fix convert amount

--- src/http/nft/nft.utils.ts
+++ src/http/nft/nft.utils.ts
@@ -9,8 +9,8 @@ export const convertFromWei = (amountInWei: string, prefix?: string): string => 
 
   if (amountInEth > 0.00009) {
     return `${parseFloat(String(Math.floor(amountInEth * 10000) / 10000))} ${prefix ? prefix : ''}ETH`;
-  } else if (amountInGwei >= 1) {
-    return `${parseFloat(amountInGwei.toFixed(2))} GWEI${prefix ? '(STETH)' : ''}`;
+  } else if (amountInGwei >= 0.00009) {
+    return `${parseFloat(String(Math.floor(amountInGwei * 10000) / 10000))} GWEI${prefix ? '(STETH)' : ''}`;
   } else {
     return `${convertedInWei} WEI${prefix ? '(STETH)' : ''}`;
   }
