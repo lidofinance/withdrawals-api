f8f5b588cb5fb63c74820b19e74af869b671258f
Andrei <t.fane@mail.ru>
2023-05-12 14:48:39 +0700

    fix: fix validate number

--- src/http/request-time/request-time.utils.ts
+++ src/http/request-time/request-time.utils.ts
@@ -3,7 +3,7 @@ import { MAX_VALID_NUMBER } from './request-time.constants';
 export const maxMinNumberValidation = (amount: string, min: string): { isValid: boolean; message?: string } => {
   const numberAmount = Number(amount);
   if (Number.isNaN(numberAmount)) return { isValid: false, message: 'Amount is not valid' };
-  if (Number.isFinite(amount)) return { isValid: false, message: 'Amount is not valid' };
+  if (!Number.isFinite(numberAmount)) return { isValid: false, message: 'Amount is not valid' };
   if (numberAmount > MAX_VALID_NUMBER) return { isValid: false, message: 'Amount is too big' };
   if (numberAmount < Number(min)) return { isValid: false, message: 'Amount is too small' };
 
