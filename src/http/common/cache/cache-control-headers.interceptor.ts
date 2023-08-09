4f8a9228ca0f402f9b211d2c2e4f6ce1b00e52df
Taras Alechin <tarens1995@gmail.ru>
2023-08-09 15:04:57 +0400

    fix: fix interceptor max-age header

--- src/http/common/cache/cache-control-headers.interceptor.ts
+++ src/http/common/cache/cache-control-headers.interceptor.ts
@@ -1,4 +1,5 @@
 import { Injectable, ExecutionContext, CallHandler, NestInterceptor } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { getCacheControlHeaders, isFunction, isNil, setCacheControlHeaders } from './cache.utils';
@@ -9,30 +10,29 @@ import {
   DEFAULT_STALE_WHILE_REVALIDATE,
 } from './cache.constants';
 import { CacheControlHeadersData } from './cache.interface';
-import { Reflector } from '@nestjs/core';
 import { CACHE_TTL_METADATA } from '@nestjs/cache-manager';
 
 @Injectable()
 export class CacheControlHeadersInterceptor implements NestInterceptor {
   constructor(protected readonly reflector: Reflector) {}
   async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
-    const ttlValueOrFactory = this.reflector.get(CACHE_TTL_METADATA, context.getHandler()) ?? null;
-    const ttl = isFunction(ttlValueOrFactory) ? await ttlValueOrFactory(context) : ttlValueOrFactory;
-
-    const {
-      maxAge = ttl,
-      staleIfError = DEFAULT_STALE_IF_ERROR,
-      staleWhileRevalidate = DEFAULT_STALE_WHILE_REVALIDATE,
-    }: CacheControlHeadersData = this.reflector.get(CACHE_CONTROL_HEADERS_METADATA, context.getHandler()) ??
-    ({} as CacheControlHeadersData);
-
-    const isNeedCacheControlHeaders = !isNil(maxAge);
-
-    if (isNeedCacheControlHeaders) {
-      setCacheControlHeaders(context, getCacheControlHeaders({ maxAge, staleIfError, staleWhileRevalidate }));
-    }
-
     try {
+      const ttlValueOrFactory = this.reflector.get(CACHE_TTL_METADATA, context.getHandler()) ?? null;
+      const ttlMilliseconds = isFunction(ttlValueOrFactory) ? await ttlValueOrFactory(context) : ttlValueOrFactory;
+      const ttl = isNil(ttlMilliseconds) ? null : Math.floor(ttlMilliseconds / 1000);
+      const {
+        maxAge = ttl,
+        staleIfError = DEFAULT_STALE_IF_ERROR,
+        staleWhileRevalidate = DEFAULT_STALE_WHILE_REVALIDATE,
+      }: CacheControlHeadersData = this.reflector.get(CACHE_CONTROL_HEADERS_METADATA, context.getHandler()) ??
+      ({} as CacheControlHeadersData);
+
+      const isNeedCacheControlHeaders = !isNil(maxAge);
+
+      if (isNeedCacheControlHeaders) {
+        setCacheControlHeaders(context, getCacheControlHeaders({ maxAge, staleIfError, staleWhileRevalidate }));
+      }
+
       return next.handle().pipe(
         tap({
           error: () => {
