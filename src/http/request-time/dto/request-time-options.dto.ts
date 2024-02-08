146eaf9052944a01e69ddc091ce62f11b3cc95b9
Taras Alekhin <tarens1995@gmail.com>
2024-02-08 10:45:08 +0100

    fix: updated ether validators

--- src/http/request-time/dto/request-time-options.dto.ts
+++ src/http/request-time/dto/request-time-options.dto.ts
@@ -3,12 +3,12 @@ import { Validate } from 'class-validator';
 import { IsEtherValidator } from 'common/validators/is-ether.validator';
 import { MaxEtherValidator } from 'common/validators/max-ether.validator';
 import { MinEtherValidator } from 'common/validators/min-ether.validator';
-import { MAX_VALID_NUMBER, MIN_VALID_NUMBER } from '../request-time.constants';
+import { MAX_VALID_ETHER, MIN_VALID_ETHER } from '../request-time.constants';
 
 export class RequestTimeOptionsDto {
   @ApiPropertyOptional({ type: 'number', minimum: 0, description: 'stETH amount' })
-  @Validate(MinEtherValidator, [MIN_VALID_NUMBER])
-  @Validate(MaxEtherValidator, [MAX_VALID_NUMBER])
+  @Validate(MinEtherValidator, [MIN_VALID_ETHER])
+  @Validate(MaxEtherValidator, [MAX_VALID_ETHER])
   @Validate(IsEtherValidator)
   readonly amount: string = '0';
 }
