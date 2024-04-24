432d05703a89c00bc11fcab92b2b253d54bde722
Taras Alekhin <tarens1995@gmail.com>
2024-04-24 15:59:13 +0200

    feat: up nodejs version, added status initialization to v1 request time endpoint

--- src/http/request-time/dto/request-time.dto.ts
+++ src/http/request-time/dto/request-time.dto.ts
@@ -1,33 +1,41 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { WaitingTimeStatus } from '../../../waiting-time';
 
 export class RequestTimeDto {
   @ApiProperty({
     example: 5,
     description: 'Maximum waiting days',
   })
-  days: number;
+  days?: number;
 
   @ApiProperty({
     example: 0,
     description: 'Queue ETH last update timestamp',
   })
-  stethLastUpdate: number;
+  stethLastUpdate?: number;
 
   @ApiProperty({
     example: 0,
     description: 'Validators last update timestamp',
   })
-  validatorsLastUpdate: number;
+  validatorsLastUpdate?: number;
 
   @ApiProperty({
     example: 10,
     description: 'Queue requests count',
   })
-  requests: number;
+  requests?: number;
 
   @ApiProperty({
     example: 0,
     description: 'Queue steth amount',
   })
-  steth: string;
+  steth?: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'Status of request calculation',
+    enum: Object.values(WaitingTimeStatus),
+  })
+  status: WaitingTimeStatus;
 }
