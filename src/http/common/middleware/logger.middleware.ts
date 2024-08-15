23e09c2452b0280f0cda69435f9cf0d25d3079e2
Taras <tarens1995@gmail.com>
2024-08-15 13:49:59 +0400

    fix: test ips array fastify (#236)

--- src/http/common/middleware/logger.middleware.ts
+++ src/http/common/middleware/logger.middleware.ts
@@ -1,20 +1,26 @@
 import { Inject, Injectable, LoggerService, NestMiddleware } from '@nestjs/common';
-import { LOGGER_PROVIDER } from '@lido-nestjs/logger';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { Request, Reply } from './interfaces';
+import { FastifyRequest } from 'fastify';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  constructor(@Inject(LOGGER_PROVIDER) protected readonly logger: LoggerService) {}
+  constructor(
+    @Inject(WINSTON_MODULE_NEST_PROVIDER)
+    private readonly logger: LoggerService,
+  ) {}
 
-  use(request: Request, reply: Reply, next: () => void) {
+  use(request: any, reply: Reply, next: () => void) {
     const { ip, method, headers, originalUrl } = request;
     const userAgent = headers['user-agent'] ?? '';
 
+    const ips = request.ips ? request.ips : [];
+
     reply.on('finish', () => {
       const { statusCode } = reply;
-      const log = { method, originalUrl, statusCode, userAgent, ip };
+      const log = { method, originalUrl, statusCode, userAgent, ip, ips };
 
-      this.logger.log('Query', log);
+      this.logger.log(JSON.stringify(log));
     });
 
     next();
