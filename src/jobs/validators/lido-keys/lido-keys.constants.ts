0bcfae9704cfb84d2c431b046239b9182fbb589f
Taras Alekhin <tarens1995@gmail.com>
2025-09-17 17:39:09 +0200

    fix: fixed hoodi keys endpoint

--- src/jobs/validators/lido-keys/lido-keys.constants.ts
+++ src/jobs/validators/lido-keys/lido-keys.constants.ts
@@ -4,5 +4,5 @@ export const KEYS_API_PATHS = {
   [CHAINS.Mainnet]: 'https://keys-api.lido.fi',
   [CHAINS.Goerli]: 'https://keys-api.testnet.fi',
   [CHAINS.Holesky]: 'https://keys-api-holesky.testnet.fi',
-  [CHAINS.Hoodi]: 'https://keys-api-holesky.testnet.fi',
+  [CHAINS.Hoodi]: 'https://keys-api-hoodi.testnet.fi',
 };
