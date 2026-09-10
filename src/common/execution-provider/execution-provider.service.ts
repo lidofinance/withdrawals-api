ac71c560e4ec63efc35590cf40de623c53a0c52a
Taras Alekhin <tarens@lido.fi>
2026-09-10 11:49:04 +0200

    fix: removed dead code

--- src/common/execution-provider/execution-provider.service.ts
+++ src/common/execution-provider/execution-provider.service.ts
@@ -1,7 +1,6 @@
 import { FallbackProviderEvents, SimpleFallbackJsonRpcBatchProvider } from '@lido-nestjs/execution';
 import { CHAINS } from '@lido-nestjs/constants';
 import { Inject, Injectable } from '@nestjs/common';
-import { ethers } from 'ethers';
 import { ConfigService } from '@nestjs/config';
 import { PrometheusService } from '../prometheus';
 import { RPC_NETWORK_NAME } from '../prometheus/prometheus.constants';
@@ -118,38 +117,6 @@ export class ExecutionProviderService {
   public async getChainId(): Promise<number> {
     const { chainId } = await this.provider.getNetwork();
     return chainId;
-  }
-
-  // using ethers.JsonRpcProvider direct request to "eth_getBlockByNumber"
-  // default @ethersproject provider getBlock does not contain "withdrawals" property
-  public async getLatestWithdrawals(): Promise<Array<{ validatorIndex: string }>> {
-    const endTimer = this.prometheusService.elRpcRequestDuration.startTimer();
-    try {
-      const block = await this.sendWithFallback('eth_getBlockByNumber', ['latest', false]);
-      endTimer({ result: 'success' });
-      return block.withdrawals;
-    } catch (error) {
-      endTimer({ result: 'error' });
-      throw error;
-    }
-  }
-
-  public async sendWithFallback(method: string, params: Array<any>) {
-    const elRpcUrls: string[] = this.configService.get('EL_RPC_URLS');
-
-    for (let i = 0; i < elRpcUrls.length; i++) {
-      const url = elRpcUrls[i];
-      try {
-        const provider = new ethers.JsonRpcProvider(url);
-        return await provider.send(method, params);
-      } catch (error) {
-        if (i === elRpcUrls.length - 1) {
-          throw error;
-        } else {
-          this.logger.warn(`RPC provider [${i}] failed. Switching to the next provider.`);
-        }
-      }
-    }
   }
 
   public async getLogsByBlockStepsWithRetry(
